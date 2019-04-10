4e423fedcf7f0b58074df480097225bc81655372
Syomx <drophere.box@gmail.com>
2019-04-10 15:59:09 +0700

    auth form on work

--- src/components/AuthPage.js
+++ src/components/AuthPage.js
@@ -2,6 +2,39 @@ import React,{Component} from 'react'
 import {connect} from 'react-redux'
 
 class AuthPage extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            first_name:'',
+            fname_err:false,
+            second_name:'',
+            sname_err:false,
+            photo:'',
+            about:''
+        };
+    
+        this.handleChangePhoto = this.handleChangePhoto.bind(this);
+        this.handleChangeFName = this.handleChangeFName.bind(this);
+        this.handleChangeSName = this.handleChangeSName.bind(this);
+        this.handleChangeAbout = this.handleChangeAbout.bind(this);
+        this.Logining = this.Logining.bind(this);
+    }
+    handleChangePhoto(event){
+        console.log(event)
+        this.setState({photo: event.target.value});
+    }
+    handleChangeFName(event){
+        this.setState({first_name: event.target.value,fname_err:false});
+    }
+    handleChangeSName(event){
+        this.setState({second_name: event.target.value,sname_err:false});
+    }
+    handleChangeAbout(event){
+        this.setState({about: event.target.value});
+    }
+    Logining(){
+        console.log(this.state)
+    }
     render(){
         return(
             <div>
@@ -10,21 +43,21 @@ class AuthPage extends Component{
                     <div className="col-lg-4">
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Your photo (url)</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter photo url" />
+                            <input type="text" className="form-control"  aria-describedby="emailHelp" placeholder="Enter photo url" value={this.state.photo} onChange={this.handleChangePhoto}/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">First name</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter first name" />
+                            <input type="text" className="form-control" aria-describedby="emailHelp" placeholder="Enter first name" value={this.state.first_name} onChange={this.handleChangeFName}/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Second name</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter second name" />
+                            <input type="text" className="form-control" aria-describedby="emailHelp" placeholder="Enter second name" value={this.state.second_name} onChange={this.handleChangeSName}/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">About</label>
-                            <textarea type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="About" />
+                            <textarea className="form-control" aria-describedby="emailHelp" placeholder="About" value={this.state.about} onChange={this.handleChangeAbout}/>
                         </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary" onClick={this.Logining()}>Submit</button>
                     </div>
                 </div>
             </div>
