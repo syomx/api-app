00ef45eb62cef778331de4185ab089cf41735f88
Syomx <drophere.box@gmail.com>
2019-04-13 19:40:11 +0700

    add new apis in list

--- src/data/api.js
+++ src/data/api.js
@@ -1,13 +1,25 @@
 export const apiData = [
     {
         logo:'https://i.pinimg.com/originals/de/1c/91/de1c91788be0d791135736995109272a.png',
-        service:'youtube',
+        service:'Youtube',
         apiKey:'123',
         description:'lorem ipsum asd as das d'
     },
     {
-        logo:'',
-        service:'behince',
+        logo:'https://png.pngtree.com/element_our/md/20180301/md_5a98273e2a951.png',
+        service:'Behance',
+        apiKey:'222',
+        description:'lorem ipsum asdasd asd as das d'
+    },
+    {
+        logo:'https://charitycatalogue.com/wp-content/uploads/2017/04/Unsplash-At-RollZign-640x427.png',
+        service:'Unsplash',
+        apiKey:'222',
+        description:'lorem ipsum asdasd asd as das d'
+    },
+    {
+        logo:'https://cdn3.iconfinder.com/data/icons/facebook-ui-flat/48/Facebook_UI-03-512.png',
+        service:'facebook',
         apiKey:'222',
         description:'lorem ipsum asdasd asd as das d'
     }
